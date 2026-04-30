cb001053dfbc26c9890984969385ada6c732b4a7
bong0219-eng <bong0219@gmail.com>
2026-05-01 08:26:11 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,4 @@
-const CACHE_NAME = 'catholic-app-v2';
-
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
-});
+const CACHE_NAME='catholic-app-v1';
+self.addEventListener('install',event=>{self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(self.clients.claim());});
+self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));});
