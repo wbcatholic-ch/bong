308e718067271c1085af6dd85be62f66e92f76d2
bong0219-eng <bong0219@gmail.com>
2026-05-13 09:51:33 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
-/* 가톨릭길동무 Service Worker - V37-6-9
+/* 가톨릭길동무 Service Worker - V21-4
    캐시를 매번 삭제하지 않고, 버전 변경 시 오래된 캐시만 정리합니다.
    localStorage/사용자 설정은 건드리지 않습니다. */
-const CACHE_VERSION = 'catholic-way-V37-6-9';
+const CACHE_VERSION = 'catholic-way-V21-4';
 const APP_SHELL = [
   './',
   './index.html',
   './diocese.html',
   './qa-firebase.html',
-  './parishes.js?v=V37-6-9',
-  './style.css?v=V37-6-9',
-  './app.js?v=V37-6-9',
-  './web.js?v=V37-6-9',
-  './prayer.js?v=V37-6-9',
-  './patches.js?v=V37-6-9',
-  './sw-update.js?v=V37-6-9',
+  './parishes.js?v=V21-4',
+  './style.css?v=V21-4',
+  './app.js?v=V21-4',
+  './web.js?v=V21-4',
+  './prayer.js?v=V21-4',
+  './patches.js?v=V21-4',
+  './sw-update.js?v=V21-4',
   './manifest.json',
   './icon-192x192.png',
   './icon-512x512.png',
