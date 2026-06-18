cd9befbe2271206bbc5702ba306c7ca67deeb2b4
wbcatholic-ch <bong0219@gmail.com>
2026-06-18 15:47:30 +0900

    Add files via upload

--- sw-update.js
+++ sw-update.js
@@ -4,7 +4,7 @@
   if(window.__APP_CACHE_LIFECYCLE_GUARD__) return;
   window.__APP_CACHE_LIFECYCLE_GUARD__ = true;
   var APP_VERSION = 'V2';
-  var SW_BUILD_VERSION = 'V8-1-9-STEP2A-GUIDE-RETURN-RESET-CHECK';
+  var SW_BUILD_VERSION = 'V8-1-10-STEP2-MENU-UNIQUE-GUARD-CHECK';
   window.APP_VERSION = APP_VERSION;
 
   function now(){ return Date.now ? Date.now() : new Date().getTime(); }
