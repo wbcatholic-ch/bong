09a4a08a191f6b74f2008651a26682abe1ff11d6
wbcatholic-ch <bong0219@gmail.com>
2026-06-19 15:53:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'catholic-way-V8-1-13-15-COVER-CARD-LINE-COLORS';
-const ASSET_VERSION = 'V8-1-13-15-COVER-CARD-LINE-COLORS';
+const CACHE_VERSION = 'catholic-way-V8-1-13-16-MODAL-BUTTON-WHITE-INSIDE';
+const ASSET_VERSION = 'V8-1-13-16-MODAL-BUTTON-WHITE-INSIDE';
 function withVersion(path) {
   return path + '?v=' + ASSET_VERSION;
 }
