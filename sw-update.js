3b8c2c69f516a2acf804c2058f092a6f1971757c
wbcatholic-ch <bong0219@gmail.com>
2026-06-20 07:06:26 +0900

    Add files via upload

--- sw-update.js
+++ sw-update.js
@@ -4,7 +4,7 @@
   if(window.__APP_CACHE_LIFECYCLE_GUARD__) return;
   window.__APP_CACHE_LIFECYCLE_GUARD__ = true;
   var APP_VERSION = 'V2';
-  var SW_BUILD_VERSION = 'V8-1-14-13-MYFAITH-OPEN-RESTORE-DIOCESE-CARD';
+  var SW_BUILD_VERSION = 'V8-1-14-14-MYFAITH-EXIT-GATE-DIOCESE-STICKY';
   window.APP_VERSION = APP_VERSION;
 
   function now(){ return Date.now ? Date.now() : new Date().getTime(); }
