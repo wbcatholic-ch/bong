7b457348361346f0b68274ea97076cb0c8add89e
wbcatholic-ch <bong0219@gmail.com>
2026-07-13 11:50:46 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,64 @@
+const CACHE_NAME = 'hanti-road-v99-cover-20260713';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './css/route-nav.css?v=20260713-v99-hanti-cover',
+  './js/route-nav.js?v=20260713-v99-hanti-cover',
+  './routes/sacred-site-landmarks.js?v=20260713-v99-hanti-cover',
+  './routes/hanti-route-data-v1.js?v=20260713-v99-hanti-cover',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png',
+  './icons/icon-512x512-maskable.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(CORE_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.map((key) => key === CACHE_NAME ? null : caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+
+  const requestUrl = new URL(request.url);
+  const scopeUrl = new URL(self.registration.scope);
+  if (requestUrl.origin !== scopeUrl.origin) return;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put('./index.html', copy)).catch(() => {});
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      const networkFetch = fetch(request).then((response) => {
+        if (response && response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(request, copy)).catch(() => {});
+        }
+        return response;
+      });
+      return cached || networkFetch;
+    })
+  );
+});
